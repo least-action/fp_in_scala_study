bb67648ea723421dedbd9c65894357634bda0800
DESKTOP-S8ADQJO\taewoo <taewoo.kim.f.r@gmail.com>
2021-02-02 23:41:11 +0900

    02/02

--- src/main/scala/Paul_Runar/chap07/Par.scala
+++ src/main/scala/Paul_Runar/chap07/Par.scala
@@ -1,14 +1,66 @@
 package Paul_Runar.chap07
 
-class Par[A] {
-}
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
 object Par {
-  def unit[A](a: => A): Par[A] = ???
-  def get[A](a: Par[A]): A = ???
+  type Par[A] = ExecutorService => Future[A]
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): C = {
-    ???
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    override def isDone: Boolean = ???
+    override def get(timeout: Long, unit: TimeUnit): A = get
+    override def isCancelled: Boolean = ???
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = ???
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def map[A,B](pa: Par[A])(f: A => B): Par[B] =
+    map2(pa, unit())((a, _) => f(a))
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call: A = a(es).get
+    })
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def asyncF[A,B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] =
+    map(parList)(_.sorted)
+
+  def simple_sequence[A](ps: List[Par[A]]): Par[List[A]] =
+    ps.foldRight(unit(List[A]()))((a, pa) => map2(a, pa)(_ :: _))
+
+  def sequenceBalanced[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
+    if (as.isEmpty) unit(Vector())
+    else if (as.length == 1) map(as.head)(a => Vector(a))
+    else {
+      val (l, r) = as.splitAt(as.length/2)
+      map2(sequenceBalanced(l), sequenceBalanced(r))(_ ++ _)
+    }
+  }
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] =
+    map(sequenceBalanced(ps.toIndexedSeq))(_.toList)
+
+  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+    def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = fork {
+    val pars: List[Par[List[A]]] =
+      as.map(asyncF(a => if (f(a)) List(a) else List()))
+    map(sequence(pars))(_.flatten)
   }
 }
-
